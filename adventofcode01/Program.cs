e2dbcbecb797f91d77a8675122972ce31ab7ceb9
Anton Andersson <62707484+anton87andersson@users.noreply.github.com>
2021-12-01 15:54:02 +0100

    Add files via upload

---
+++ adventofcode01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace adventofcode01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prev_num = 0;
+            int count_inc = -1;
+
+            foreach (string line in System.IO.File.ReadLines("PATH/TO/FILE"))
+            {
+                int line_to_int = int.Parse(line);
+
+                if (line_to_int > prev_num)
+                {
+                    Console.WriteLine("Not bigger");
+                    count_inc++;
+                } 
+
+                prev_num = line_to_int;
+               
+            }
+
+            Console.WriteLine(count_inc.ToString());
+            Console.ReadLine();
+        }
+    }
+}
